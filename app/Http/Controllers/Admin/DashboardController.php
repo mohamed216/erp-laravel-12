5b7e36bf09c486e7cc2269cf3bfd3fce6cf768d8
mohamed216 <mohamed216@gmail.com>
2026-03-02 20:56:38 +0000

    Fix: add auth middleware to controllers

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -10,6 +10,11 @@ use App\Models\Order;
 
 class DashboardController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $stats = [
--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index(Request $request)
     {
         $users = User::when($request->search, fn($q, $s) => $q->where('name', 'like', "%$s%"))->paginate(10);
