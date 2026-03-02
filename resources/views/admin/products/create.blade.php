c87548fa0d89e5aa47b3dc7fadead62056013607
mohamed216 <mohamed216@gmail.com>
2026-03-02 22:59:36 +0000

    Fix product create view

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -2,10 +2,27 @@
 @section('title', 'Create Product')
 @section('content')
 <form method="POST" action="/products">@csrf
-<input name="name" class="form-control mb-2" placeholder="Name" required>
-<input name="sku" class="form-control mb-2" placeholder="SKU" required>
-<input name="price" type="number" class="form-control mb-2" placeholder="S_price" required>
-<input name="stock" type="number" class="form-control mb-2" placeholder="Stock" value="0">
-<button class="btn btn-primary">Create</button>
+<div class="row">
+    <div class="col-md-6">
+        <input name="name" class="form-control mb-2" placeholder="اسم المنتج" required>
+    </div>
+    <div class="col-md-6">
+        <input name="sku" class="form-control mb-2" placeholder="رمز المنتج" required>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-6">
+        <input name="price" type="number" step="0.01" class="form-control mb-2" placeholder="سعر البيع" required>
+    </div>
+    <div class="col-md-6">
+        <input name="cost_price" type="number" step="0.01" class="form-control mb-2" placeholder="سعر التكلفة">
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-6">
+        <input name="stock" type="number" class="form-control mb-2" placeholder="الكمية" value="0">
+    </div>
+</div>
+<button class="btn btn-primary">إنشاء</button>
 </form>
 @endsection
