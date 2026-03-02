acd12f355f8d4d7a9103faeb27cecbfbe022d534
mohamed216 <mohamed216@gmail.com>
2026-03-02 21:11:35 +0000

    Add OrderItem model and migration

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -18,4 +18,9 @@ class Order extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -13,4 +13,9 @@ class Product extends Model
     {
         return $this->hasOne(Inventory::class);
     }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
