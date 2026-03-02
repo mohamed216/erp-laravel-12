afed34886073e46862e9ecd2eb05ff9001a89dd6
mohamed216 <mohamed216@gmail.com>
2026-03-02 23:28:55 +0000

    Fix language switcher with middleware

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,21 @@ use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\InventoryController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::post('/logout', [AuthController::class, 'logout']);
+
+// Language switcher
+Route::get('/lang/{locale}', function ($locale) {
+    if (in_array($locale, ['ar', 'en'])) {
+        session(['locale' => $locale]);
+        app()->setLocale($locale);
+    }
+    return back();
+});
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/', fn() => redirect('/dashboard'));
@@ -22,10 +32,4 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('products', ProductController::class);
     Route::resource('orders', OrderController::class);
     Route::resource('inventory', InventoryController::class)->except(['show', 'destroy', 'create', 'store']);
-});
-
-Route::get('/lang/{locale}', function ($locale) {
-    session(['locale' => $locale]);
-    app()->setLocale($locale);
-    return back();
 });
